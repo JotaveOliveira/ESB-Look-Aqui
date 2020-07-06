d5000635d66d4c10ddd821a5232942fe38922aa4
Joao Vitor Oliveira de Almeida <jvitoroliveira@brq.com>
2020-07-06 18:25:31 -0300

    Subindo ESB Git

---
+++ src/main/java/br/com/look/aqui/EsbLookAquiiApplication.java
@@ -0,0 +1,13 @@
+package br.com.look.aqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbLookAquiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbLookAquiiApplication.class, args);
+	}
+
+}
